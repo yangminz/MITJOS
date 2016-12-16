b4ed5b9bb7fb54724c1221c1d923bc3841a9066d
yangminz <yangminzhao14@gmail.com>
2016-12-17 00:10:42 +0800

    exercise 1

--- kern/env.c
+++ kern/env.c
@@ -437,20 +437,22 @@ void
 env_create(uint8_t *binary, enum EnvType type)
 {
 	// LAB 3: Your code here.
-
-	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
-	// LAB 5: Your code here.
     int ret = 0;
     struct Env *e = NULL;
     ret = env_alloc(&e, 0);
 
-    if(ret < 0)
-    {
+    if(ret < 0){
         panic("env_create: %e\n", ret);
     }
 
     load_icode(e, binary);
     e->env_type = type;
+
+	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
+	// LAB 5: Your code here.
+    if(type == ENV_TYPE_FS){
+        e->env_tf.tf_eflags |= FL_IOPL_MASK;
+    }
 }
 
 //
